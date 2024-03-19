432ce97556ecdc32810da1f2b210291e7fc531b3
Myriam Lecaçon <malys_jay@hotmail.com>
2024-03-19 18:51:29 +0100

    1er get test, ajout de l'image de la canette Planty

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Planty</title>
+</head>
+<body>
+    <img src= "images/canette Planty.png" alt= "la Canette de Planty" />
+</body>
+</html>
